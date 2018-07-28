e449c8ca26aab472d07826cae03231dbd76ba2a6
Francis Breidenbach <fbreidenbach@gmail.com>
2018-07-27 19:28:07 -0700

    Create SearchCard and try to work on it ...

---
+++ src/components/SearchCard.js
@@ -0,0 +1,14 @@
+import React, { PureComponent } from "react";
+import { StyleSheet, View, Text, TextInput } from "react-native";
+
+export default class SearchCard extends PureComponent {
+  render() {
+    return (
+      <View>
+        <Text>
+          <TextInput />
+        </Text>
+      </View>
+    );
+  }
+}
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -16,9 +16,7 @@ export default class HomeScreen extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "orange",
-    justifyContent: "center",
-    alignItems: "center"
+    backgroundColor: "orange"
   },
   contentContainer: {
     fontSize: 20,
