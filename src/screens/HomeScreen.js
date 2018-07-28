3c2bc89ad4c4ee0d93766d994deba72a33877821
Francis Breidenbach <fbreidenbach@gmail.com>
2018-07-27 18:59:28 -0700

    Create HomeScreen and move elements there.

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import { StyleSheet, View, Text } from "react-native";
+
+export default class HomeScreen extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.contentContainer}>
+          <Text>Search for Trumpisms!</Text>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "orange",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  contentContainer: {
+    fontSize: 20,
+    textAlign: "center",
+    margin: 10
+  }
+});
