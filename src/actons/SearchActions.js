2ffd060cd3adc04acfe264fac8ab553f56ccc871
Francis Breidenbach <fbreidenbach@gmail.com>
2018-07-31 20:53:53 -0700

    Create SearchActions

---
+++ src/actons/SearchActions.js
@@ -0,0 +1,25 @@
+import { FETCH_SEARCH_RESULTS } from "./types";
+
+const path = query => `https://api.tronalddump.io/search/quote?query=${query}`;
+
+export const fetchSearchResults = query => {
+  const requestUrl = path(query);
+
+  return dispatch => {
+    fetch(requestUrl)
+      .then(response => response.json())
+      .then(json => {
+        const { quotes } = json._embedded;
+
+        const data = {
+          results: quotes,
+          userQuery: query
+        };
+
+        dispatch({
+          type: FETCH_SEARCH_RESULTS,
+          payload: data
+        });
+      });
+  };
+};
